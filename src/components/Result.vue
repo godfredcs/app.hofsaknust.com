385ece7a6ed80651230d769e5a44065b918020a7
godfredcs <addaiz.godfred@gmail.com>
2018-06-07 03:11:03 +0000

    Hooking up socket io in but experiencing cors issues

---
+++ src/components/Result.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="result">
+        <p>counter is {{ counter }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        counter() {
+            return this.$store.state.counter;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .result {
+        margin: 100px;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { store } from './store'
+
+import socketio from 'socket.io-client';
+import VueSocketIO from 'vue-socket.io';
+
+export const SocketInstance = socketio('http://localhost:5000');
+
+Vue.use(VueSocketIO, SocketInstance);
 
 Vue.config.productionTip = false
 
@@ -10,6 +18,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
