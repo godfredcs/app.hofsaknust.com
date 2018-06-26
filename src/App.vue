5cbae9d7044c9dfc4d58d88ff59fcc3c9159bafa
Godfred Boateng Addai <addaiz.godfred@gmail.com>
2018-06-26 16:10:09 +0000

    created login component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <router-view/>
-    <p>{{ isConnected ? 'The user is connected' : 'Offline' }}</p>
+    <!-- <p>{{ isConnected ? 'The user is connected' : 'Offline' }}</p>
 
-    <button @click="pingServer()">Ping server</button>
+    <button @click="pingServer()">Ping server</button> -->
   </div>
 </template>
 
@@ -49,6 +49,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/Login'
+import Register from '@/components/Register'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
@@ -7,10 +9,9 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    { path: '', name: 'Home', component: HelloWorld },
+    { path: '/login', name: 'Login', component: Login },
+    { path: '/register', name: 'Register', component: Register },
+    { path: '*', redirect: { name: 'Home' } }
   ]
 })
